17a04f865d0f7b8753990e6e5946177bbe5e9f82
William1Wu <jay79227@gmail.com>
2019-11-25 23:25:18 +0800

    Update WilliamWu.cpp

--- (2019.11.25) 771. Jewels and Stones/WilliamWu.cpp
+++ (2019.11.25) 771. Jewels and Stones/WilliamWu.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     int numJewelsInStones(string J, string S) {
         
-        int ans = 0;
+        int result = 0;
         if(J=="" || S=="")
-            ans;
-                
+            return result;
+        
         std::map<char, int> table;
-        for(int i = 0; i < J.size(); i++)
-            table.insert(table.begin(), std::pair<char,int>(J[i], 0));
-        
-        for(int j = 0; j < S.size(); j++)
-        {
-            std::map<char,int>::iterator iterator = table.find(S[j]);
-            if (iterator != table.end())
-                iterator->second++;
-        }
-        
-        for(map<char, int>::iterator it = table.begin(); it!=table.end(); it++)
-            ans+= it->second;
+        for(int i = 0; i < S.size(); i++)
+            table[S[i]]++;
+                 
+        for(int j = 0; j < J.size(); j++)
+            result += table[J[j]];
             
-        return ans;        
+        return result;        
     }
 };
