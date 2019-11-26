494a8270f8a441b06b49b6492176fd3428fb33ee
Chanone-Lin <58161088+Chanone-Lin@users.noreply.github.com>
2019-11-26 18:34:01 +0800

    Add files via upload

---
+++ (2019.11.26) 905. Sort Array By Parity/ChanoneLin.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        vector<int> even(A.size()), odd(A.size());        
+        int evenpos=0,oddposs=0;
+        
+        for(int i=0;i<A.size();i++){
+            if(A[i]%2==0){                
+                even[evenpos]=A[i];
+                evenpos=evenpos+1;
+            }
+            else{
+                odd[oddposs]=A[i];
+                oddposs=oddposs+1;
+            }
+            
+        }        
+        vector<int> result(A.size());
+        for(int p=0;p<evenpos;p++){
+            result[p]=even[p];
+        }
+        for(int q=0;q<oddposs;q++){
+            result[evenpos+q]=odd[q];
+        }
+        return result;
+    }
+};
