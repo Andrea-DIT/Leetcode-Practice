c6188fc3d72b92a694e2e41909370b5bebd9ed18
Chanone-Lin <58161088+Chanone-Lin@users.noreply.github.com>
2019-11-27 18:26:28 +0800

    Add files via upload

---
+++ (2019.11.27) 832. Flipping an Image/ChanoneLin.cpp
@@ -0,0 +1,26 @@
+﻿class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {       
+        
+        int i,j,k;
+        for(i=0;i<A.size();i++){            
+            for(j=0,k=A[i].size()-1;j<k;j++,k--){                
+                int tmp=A[i][j];
+                A[i][j]=A[i][k];
+                A[i][k]=tmp;
+                }               
+        }
+        for(i=0;i<A.size();i++){
+            for(j=0;j<A[0].size();j++){
+                if(A[i][j]==0)
+                    A[i][j]=1;
+                     else
+                     A[i][j]=0;
+                
+            }
+        }
+            
+        return A;
+        
+    }
+};
