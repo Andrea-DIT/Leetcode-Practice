3c8d0014daae2ea90e2c2ac31b71d4d266247a7a
jeffrey2290 <wowwow12345678987654321@yahoo.com.tw>
2019-12-05 18:14:39 +0800

    add Jeffrey's answer

---
+++ (2019.12.5) 136. Single Number/JeffreyHuang.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int output = 0;
+        
+        for (const auto &num : nums)
+        {
+            output ^= num;
+        }
+        
+        return output;
+    }
+};
