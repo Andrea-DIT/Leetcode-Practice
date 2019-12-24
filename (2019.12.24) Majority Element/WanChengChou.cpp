4ccf2e7d98ca36cf834dc6e2fe08f8df6f61f0f4
WanChengChou <45063424+WanChengChou@users.noreply.github.com>
2019-12-24 23:04:22 +0800

    Update WanChengChou.cpp

--- (2019.12.24) Majority Element/WanChengChou.cpp
+++ (2019.12.24) Majority Element/WanChengChou.cpp
@@ -3,16 +3,14 @@ public:
     int majorityElement(vector<int>& nums)
     {
         map<int, int> counting;
-        int answer;
         int size = nums.size();
         for(int i=0; i<size; i++)
         {
             if(++counting[nums[i]] > size/2)
             {
-                answer = nums[i];
-                continue;
+                return nums[i];
             }
         }
-        return answer;
+        return nums[0];
     }
 };
