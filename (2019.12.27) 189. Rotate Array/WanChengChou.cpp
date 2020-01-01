ba543b82d45db468c497a9da16fbebfa80479232
WanChengChou <45063424+WanChengChou@users.noreply.github.com>
2020-01-02 00:34:53 +0800

    Add files via upload

---
+++ (2019.12.27) 189. Rotate Array/WanChengChou.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k)
+    {
+        k = k % nums.size();
+        if(k == 0) return;
+        nums.insert(nums.begin(), nums.end()-k, nums.end());
+        nums.erase(nums.end()-k, nums.end());
+    }
+};
